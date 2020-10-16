efbc42dcf3b12a13e22232a6d9b708f69083d230
husseinbasha <37039156+husseinbasha@users.noreply.github.com>
2020-10-16 04:08:15 +0300

    Update addarticle.inc.php

---
+++ includes/addarticle.inc.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+if(isset($_SESSION['ID'])){
+    $id = $_SESSION['ID'];
+
+if(!empty($_POST['submit'])){
+    $title=$_POST['title'];
+    $image=$_POST['image'];
+    $content=$_POST['article'];
+    $author=$_SESSION['USERNAME'];
+   
+    
+if(empty($title)||empty($article)){
+    echo '<div class="alert alert-danger">All fileds are requierd!</div>';
+}else{
+    addArticle($mysqli, $id, $author, $title, $content);
+    header('Location:article.php?inserted');
+}
+}
+}else{
+    header('Location: ../landing.php');
+}
