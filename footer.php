8c7a09483324e6a34a1a4449c21757380a75a30a
Firas Muhsen Al-Azizy <firasazizy614@gmail.com>
2020-10-16 20:52:13 +0300

    add footer content

--- footer.php
+++ footer.php
@@ -3,8 +3,36 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <script src="https://kit.fontawesome.com/0e10084a8f.js" crossorigin="anonymous"></script>
-        
+        <script src="https://kit.fontawesome.com/0e10084a8f.js" crossorigin="anonymous"></script>  
 </body>
-<footer></footer>
+<footer class="container-fluid text-center">
+    <style>
+        footer {
+  color: #DEDEDE;
+  background-color: #120D03;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-top: 160px;
+  font-size: 20px;
+
+}
+footer a {
+  color: white !important;
+  text-decoration:none;
+}
+
+
+footer a:hover {
+ color: #76b852 !important; 
+}
+
+
+    </style>
+    <div class="container" >
+        <a class="navbar-brand">
+            <i class="fas fa-brain"></i> FEEDIT
+        </a>
+        <em> created with love by <space>   </space><a href="https://github.com/husseinbasha/">Hussain</a><space>   </space>  <a href="https://github.com/HebaFayad">Heba</a> <space>   </space><a href="https://github.com/Firas-Al-Azizy">Firas</a> <span>   </span> 2020 <br> </em>     
+    </div>
+    </footer>
 </html>
